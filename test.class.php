a383c52e8513a9c4f45db608fd7aa4de2fe48bef
wangjun2012al <wangjun2012al@gmail.com>
2014-10-23 19:42:27 +0800

    1111

---
+++ test.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ * @authors Wang Jun (wangjun2012al@gmail.com)
+ * @date    2014-10-23 19:35:33
+ * @link 	https://github.com/wangjun2012al/PHP.git 
+ * @version $Id$
+ */
+
+class ClassName{
+    
+    function __construct(){
+        
+    }
+}
